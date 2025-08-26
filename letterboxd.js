d5a8d1e10bcc300bbf0649565f797de71f83d8de
HashimMatloob <hashimraja67@gmail.com>
2025-08-26 18:34:35 +0500

    Scraper file for letterboxd

--- letterboxd.js
+++ letterboxd.js
@@ -1,33 +1,52 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-async function main() {
-  try {
-    const response = await axios.get("https://letterboxd.com/tiktokTaste/films/");
-    const $ = cheerio.load(response.data);
 
+async function scrapeMovies(url) {
+  try {
+    const { data } = await axios.get(url);
+    const $ = cheerio.load(data);
     const movies = [];
 
-    $("[data-component-class='globals.comps.LazyPoster']").each((i, el) => {
+    $(".react-component[data-component-class='globals.comps.LazyPoster']").each((i, el) => {
       const title = $(el).attr("data-item-name");
       const fullTitle = $(el).attr("data-item-full-display-name");
       const link = "https://letterboxd.com" + $(el).attr("data-item-link");
-      const year = fullTitle.match(/\((\d{4})\)/)?.[1]; // extract year if present
+      const year = fullTitle?.match(/\((\d{4})\)/)?.[1];
 
       if (title) {
-        movies.push({
-          title,
-          fullTitle,
-          year,
-          link
-        });
+        movies.push({ title, fullTitle, year, link });
       }
     });
 
-    console.log("Movies found:", movies);
-  } catch (error) {
-    console.error(error);
+    return movies;
+  } catch (err) {
+    console.error("Scraping error:", err.message);
+    return [];
   }
 }
 
-main();
+async function getLetterboxdProfile(username, maxPages = 5) {
+  let page = 1;
+  let watched = [];
+
+  while (page <= maxPages) {
+    const url = `https://letterboxd.com/${username}/films/page/${page}/`;
+    console.log(`Fetching ${url}`);
+    const pageMovies = await scrapeMovies(url);
+    if (pageMovies.length === 0) break; 
+    watched.push(...pageMovies);
+    page++;
+  }
+
+
+  const liked = await scrapeMovies(`https://letterboxd.com/${username}/likes/films/`);
+
+  return { watched, liked };
+
+}
+
+
+
+
+module.exports = { getLetterboxdProfile };
